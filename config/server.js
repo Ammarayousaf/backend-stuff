9a5f5e1a3af3d04269c320a98387efff1ec7d34a
Ammarayousaf <58362239+Ammarayousaf@users.noreply.github.com>
2021-03-25 00:18:59 +0100

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2bd9136932b26fdbc7c7ed76d486e620'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ada8ef1a-8cdd-472f-a1ee-40fc207d9dfd'
+};
